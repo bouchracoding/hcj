55008828499c9cb7b653acf87aab3283e11aa620
bouchracoding <152641287+bouchracoding@users.noreply.github.com>
2024-01-11 17:04:06 +0100

    first commit

---
+++ car.js
@@ -0,0 +1,9 @@
+let img = document.querySelector(".img");
+let container = document.querySelector(".container");
+function cars(car){
+    img.src = car;   
+}
+function colors(color){
+    container.style.background = color;
+
+}
